779d79e2b5aebafaed34f1d3d646ac66a61b4bd7
panvena-actions <panvena-actions@users.noreply.github.com>
2025-12-31 23:57:19 +0000

    chore: bump version to 0.8.22

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 import os
 
 # Get version from source
-VERSION = "0.8.21"
+VERSION = "0.8.22"
 with open("SteamAchievementLocalizer.py", "r", encoding="utf-8") as f:
     for line in f:
         if "APP_VERSION" in line:
